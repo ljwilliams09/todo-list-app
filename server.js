98d32429f5fa57c18bd42e82763a9453db607ce6
Luca Williams <ljwilliams@colgate.edu>
2025-07-04 16:32:55 -0400

    setting up server

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-const express = require('express')
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) =>{
+    res.send("Welcome to the homepage!")
+});
+
+app.get('/login', (req, res) =>{
+    res.send("Welcome to the login page!")
+});
+
+app.get('/signup', (req, res)=>{
+    res.send("Welcome to the signup page!")
+});
